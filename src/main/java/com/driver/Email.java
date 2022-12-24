8399bafe88982319696d83df2197d17af2d8ab14
PravinElangovan <pravinkumarelangovan20@gmail.com>
2022-12-24 15:45:06 +0530

    first commit

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -1,17 +1,55 @@
 package com.driver;
 
+import java.util.Date;
+
 public class Email {
 
     private String emailId;
     private String password;
+
+    private Date date;
+
+    private String sender;
+
+    private String message;
 
     public Email(String emailId){
         this.emailId = emailId;
         this.password = "Accio@123";
     }
 
+    public Email(Date date,String sender,String message){
+        this.date=date;
+        this.sender = sender;
+        this.message =message;
+    }
+
+    public void setDate(Date date){
+        this.date = date;
+    }
+    public Date getDate(){
+        return date;
+    }
+    public void setSender(String sender){
+        this.sender = sender;
+    }
+    public String getSender(){
+        return sender;
+    }
+    public void setMessage(String message){
+        this.message = message;
+    }
+    public String getMessage(){
+        return message;
+    }
+
+
+
     public String getEmailId() {
         return emailId;
+    }
+    public void setPassword(String password){
+        this.password = password;
     }
 
     public String getPassword() {
@@ -19,11 +57,28 @@ public class Email {
     }
 
     public void changePassword(String oldPassword, String newPassword){
+
         //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
         // 1. It contains at least 8 characters
         // 2. It contains at least one uppercase letter
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        if(oldPassword.equals(getPassword())){
+            //check if the new password meets the required criteria
+            if(newPassword.length() >= 8 &&
+                    newPassword.matches(".*[A-Z].*") &&
+                    newPassword.matches(".*[a-z].*") &&
+                    newPassword.matches(".*[0-9].*") &&
+                    newPassword.matches(".*[^A-Za-z0-9].*")){
+                setPassword(newPassword);
+                System.out.println("Password changed successfully");
+            }else{
+                System.out.println("New password doesn't meet the required criteria. Please try again.");
+            }
+        }else{
+            System.out.println("Old password is incorrect. Please try again.");
+        }
+
     }
 }
--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -5,18 +5,25 @@ import org.apache.commons.lang3.tuple.Pair;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
 public class Workspace extends Gmail{
 
     private ArrayList<Meeting> calendar; // Stores all the meetings
 
+
     public Workspace(String emailId) {
+
         // The inboxCapacity is equal to the maximum value an integer can store.
+        super(emailId,Integer.MAX_VALUE);
+        this.calendar = new ArrayList<>();
+
 
     }
 
     public void addMeeting(Meeting meeting){
         //add the meeting to calendar
+        calendar.add(meeting);
 
     }
 
@@ -25,6 +32,20 @@ public class Workspace extends Gmail{
         // 1. At a particular time, you can be present in at most one meeting
         // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
         // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
-
+        int maximumMeetingCanAttened = 0;
+        LocalTime endTime =LocalTime.MIN;
+        Collections.sort(calendar, new Comparator<Meeting>() {
+            @Override
+            public int compare(Meeting m1, Meeting m2) {
+                return m1.getStartTime().compareTo(m2.getStartTime());
+            }
+        });
+        for(Meeting meeting : calendar){
+            if(meeting.getStartTime().isAfter(endTime)){
+                maximumMeetingCanAttened++;
+                endTime = meeting.getEndTime();
+            }
+        }
+        return maximumMeetingCanAttened ;
     }
 }
